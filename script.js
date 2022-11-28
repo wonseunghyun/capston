ab568d8bca02ac218bd0688973c39bafc6b455db
원승현 <wonseung@wonseunghyeon-ui-MacBookAir.local>
2022-11-28 19:47:33 +0900

    donee

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let swiper = new Swiper('.swiper-container', {
+let swiper = new Swiper('#swiper-1', {
   slidesPerView: 1,
   spaceBetween: 10,
   breakpoints:{
@@ -13,7 +13,31 @@ let swiper = new Swiper('.swiper-container', {
     clickable:true,
   },
   navigation:{
-    nextEl:".swiper-button-next",
-    prevEl:".swiper-button-prev",
+    nextEl:"#sbn1",
+    prevEl:"#sbp1",
   },
+  observer:true,
+  observeParents:true,
   });
+
+  let swiper2 = new Swiper('#swiper-2', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints:{
+      900:{slidesPerView:3,}},
+    autoplay:{
+      delay:3000,
+      disableOnInteraction:false,},
+    loop:false,
+    loopAdditionalSlides: 1,
+    pagination:{
+      el:"#swiper-pagination",
+      clickable:true,
+    },
+    navigation:{
+      nextEl:"#sbn2",
+      prevEl:"#sbp2",
+    },
+    observer:true,
+    observeParents:true,
+    });
